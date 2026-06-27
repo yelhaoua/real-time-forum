82fda154760c68a5b1b13316e3b99dd938a82db8
yelhaoua <yacobelhaouarii@gmail.com>
2026-06-27 15:24:03 +0100

    creat routes folder

---
+++ frontend/app.js
@@ -0,0 +1,7 @@
+import registerForm from "./js/componentes/register-form.js";
+
+const mainPage = document.getElementById("page");
+
+console.log(registerForm());
+mainPage.innerHTML = registerForm();
+console.log(mainPage);

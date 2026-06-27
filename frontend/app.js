8fb4738b8606701d198245c24ff0a5178d8e24e7
yelhaoua <yacobelhaouarii@gmail.com>
2026-06-27 15:10:49 +0100

    test logic single page

--- frontend/app.js
+++
@@ -1,3 +0,0 @@
-import registerForm from "./js/componentes/register-form";
-
-document.getElementById("page").innerHTML = registerForm();
--- frontend/js/componentes/register-form.js
+++
@@ -1,3 +0,0 @@
-export default function registerForm() {
-  return `<div> Helooo </div>`;
-}

4754051f2c09414db2e084b5482d37d203cc7c00
yelhaoua <yacobelhaouarii@gmail.com>
2026-06-27 15:29:03 +0100

    add pathe name detection

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,11 @@
+import NotFoundPage from "./js/componentes/not-found.js";
 import registerForm from "./js/componentes/register-form.js";
+import routes from "./js/routes/routes.js";
 
 const mainPage = document.getElementById("page");
 
+const pathe = window.location.pathname;
+
 console.log(registerForm());
-mainPage.innerHTML = registerForm();
+mainPage.innerHTML = routes[pathe] || NotFoundPage();
 console.log(mainPage);

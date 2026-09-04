2e1c0f242a0eee6fd962ae2ef1cddcfd4ab92907
yelhaoua <yacobelhaouarii@gmail.com>
2026-09-04 12:00:58 +0100

    refactore code

--- frontend/js/componentes/home-page.js
+++
@@ -1,7 +0,0 @@
-import FeedPage from "./postes/feed.js";
-import NavBar from "./nave-bare.js";
-import CardPost from "./ui/card-post.js";
-
-export default function HomePage() {
-  return FeedPage();
-}
--- frontend/js/componentes/ui/commentsBox.js
+++
@@ -1,6 +0,0 @@
-export default function CommentsBox() {
-    const commentsBox = document.createElement('div');
-    commentsBox.id = "commentsBox"
-    const container = document.getElementById("card-container");
-    container.appendChild(commentsBox)
-}
